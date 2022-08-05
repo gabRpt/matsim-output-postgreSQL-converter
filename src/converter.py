18e3d37bb007bc04fd5d4b3920d210f45589fafc
gabRpt <33011305+gabRpt@users.noreply.github.com>
2022-08-05 16:32:58 +0200

    Added option to import detailed_network.csv file when importing network links

--- src/converter.py
+++ src/converter.py
@@ -78,40 +78,43 @@ def importNetworkLinks(useDetailedNetworkFile=True):
     links = network.links
     linkAttributes = network.link_attrs
     
+    
+    # Creating lines in links from "from_node" and "to_node" coordinates
+    # attach xy to links
+    full_net = (links
+    .merge(nodes,
+            left_on='from_node',
+            right_on='node_id')
+    .merge(nodes,
+            left_on='to_node',
+            right_on='node_id',
+            suffixes=('_from_node', '_to_node'))
+    )
+
+    # create the geometry column from coordinates
+    geometry = [LineString([(ox,oy), (dx,dy)]) for ox, oy, dx, dy in zip(full_net.x_from_node, full_net.y_from_node, full_net.x_to_node, full_net.y_to_node)]
+
+    # build the geopandas geodataframe
+    links = (gpd.GeoDataFrame(full_net,
+        geometry=geometry)
+        .drop(columns=['x_from_node','y_from_node','node_id_from_node','node_id_to_node','x_to_node','y_to_node'])
+    )
+    
+    # Conversion of the geometry column to object
+    links['geom'] = links['geometry'].apply(lambda x: x.wkt) # creating a new geom column to avoid the error "Geometry column does not contain geometry."
+    links.drop(columns=['geometry'], inplace=True)
+    
     if useDetailedNetworkFile:
         detailedNetworkDataframe = pd.read_csv(config.PATH_DETAILED_NETWORK, sep=',')
         
         # Removing rows where the linestring has less than 2 coordinates
         detailedNetworkDataframe = detailedNetworkDataframe[detailedNetworkDataframe['Geometry'].apply(lambda x: len(x.split(',')) > 1)]
-        
         detailedNetworkDict = dict(zip(detailedNetworkDataframe['LinkId'], detailedNetworkDataframe['Geometry']))
         
         # adding the geometry of the links found in the detailed network file to the links found in the network file
         for link in links.itertuples():
             if link.link_id.isdigit() and int(link.link_id) in detailedNetworkDict:
-                links.loc[link.Index, 'geometry'] = detailedNetworkDict[int(link.link_id)]
-        print(links.loc[links['link_id'] == '100002'])
-    else:
-        # Creating lines in links from "from_node" and "to_node" coordinates
-        # attach xy to links
-        full_net = (links
-        .merge(nodes,
-                left_on='from_node',
-                right_on='node_id')
-        .merge(nodes,
-                left_on='to_node',
-                right_on='node_id',
-                suffixes=('_from_node', '_to_node'))
-        )
-
-        # create the geometry column from coordinates
-        geometry = [LineString([(ox,oy), (dx,dy)]) for ox, oy, dx, dy in zip(full_net.x_from_node, full_net.y_from_node, full_net.x_to_node, full_net.y_to_node)]
-
-        # build the geopandas geodataframe
-        links = (gpd.GeoDataFrame(full_net,
-            geometry=geometry)
-            .drop(columns=['x_from_node','y_from_node','node_id_from_node','node_id_to_node','x_to_node','y_to_node'])
-        )
+                links.loc[link.Index, 'geom'] = detailedNetworkDict[int(link.link_id)]
     
     
     # Renaming the attributes columns to match the database
@@ -147,20 +150,13 @@ def importNetworkLinks(useDetailedNetworkFile=True):
     # Renaming the columns to match the database
     links.rename(columns={
         'link_id': 'id',
-        'geometry': 'geom',
     }, inplace = True)
     
     for columnName in attributesColumnsNames:
         links.rename(columns={
             columnName: columnName.replace(':', '_')
         }, inplace = True)
-    
-    
-    # Conversion of the geometry column to object
-    if not useDetailedNetworkFile:
-        links['geom'] = links['geom'].apply(lambda x: x.wkt)
-    
-    
+        
     # Importing the data to the database
     conn = tools.connectToDatabase()
     links.to_sql('networkLink', con=conn, if_exists='append', index=False, dtype={'geom': Geometry('LINESTRING', srid=config.DB_SRID)})
