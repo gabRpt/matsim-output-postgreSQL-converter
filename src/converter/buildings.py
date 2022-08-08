c1d4f77e53a1ab82bd53abc7771b7621450e4c16
gabRpt <33011305+gabRpt@users.noreply.github.com>
2022-08-08 16:33:00 +0200

    Simplified the code to import building

--- src/converter/buildings.py
+++ src/converter/buildings.py
@@ -7,12 +7,11 @@ import json
 from geoalchemy2 import Geometry
 
 def importBuildings():
-    print('')
+    print('') #TODO remove this line
     
     with open(config.PATH_BUILDINGS, 'r') as buildingsJson:
         buildings = json.load(buildingsJson)        
         polygonFeaturesDict = collections.defaultdict(list)
-        multiPolygonFeaturesDict = collections.defaultdict(list)
         
         for feature in buildings['features']:
             # Checking if the feature has coordinates
@@ -20,42 +19,29 @@ def importBuildings():
                 continue
             
             # Checking supported geometry type
-            if feature['geometry']['type'] == 'Polygon':
+            if feature['geometry']['type'] in ['Polygon', 'MultiPolygon']:
                 polygonString = _convertCoordinatesToPolygon(feature['geometry']['coordinates'])
-                polygonFeaturesDict['geometryType'].append(feature['geometry']['type'])
-                polygonFeaturesDict['type'].append(feature['type'])
-                polygonFeaturesDict['PK'].append(feature['properties']['PK'])
-                polygonFeaturesDict['height'].append(feature['properties']['HEIGHT'])         
-                polygonFeaturesDict['geometry'].append(polygonString)
-                
-            elif feature['geometry']['type'] == 'MultiPolygon':
-                polygonString = _convertCoordinatesToPolygon(feature['geometry']['coordinates'])
-                multiPolygonFeaturesDict['geometryType'].append(feature['geometry']['type'])
-                multiPolygonFeaturesDict['type'].append(feature['type'])
-                multiPolygonFeaturesDict['PK'].append(feature['properties']['PK'])
-                multiPolygonFeaturesDict['height'].append(feature['properties']['HEIGHT'])         
-                multiPolygonFeaturesDict['geometry'].append(polygonString)
                 
             else:
                 print(f'WARNING: geometry type "{feature["geometry"]["type"]}" not supported. Skipping feature.')
                 continue
-            
-               
-            # Polygon coordinates structure in the json file is: [[[x,y], [x,y], [x,y], [x,y], [x,y]]]
-            # We need to convert it to POLYGON((x y, x y, x y, x y, x y))
-            
         
+            polygonFeaturesDict['geometryType'].append(feature['geometry']['type'])
+            polygonFeaturesDict['type'].append(feature['type'])
+            polygonFeaturesDict['PK'].append(feature['properties']['PK'])
+            polygonFeaturesDict['height'].append(feature['properties']['HEIGHT'])         
+            polygonFeaturesDict['geometry'].append(polygonString)
+            
         polygonDataframe = pd.DataFrame(polygonFeaturesDict)
         print(polygonDataframe)
         
-        multiPolygonDataframe = pd.DataFrame(multiPolygonFeaturesDict)
-        print(multiPolygonDataframe)
-        
         conn = tools.connectToDatabase()
         polygonDataframe.to_sql('building', con=conn, if_exists='append', index=False, dtype={'geom': Geometry('POLYGON', srid=config.DB_SRID)})
-        multiPolygonDataframe.to_sql('building', con=conn, if_exists='append', index=False, dtype={'geom': Geometry('MULTIPOLYGON', srid=config.DB_SRID)})
 
 
+
+# Polygon coordinates structure in the json file is: [[[x,y], [x,y], [x,y], [x,y], [x,y]]]
+# We need to convert it to POLYGON((x y, x y, x y, x y, x y))
 def _convertCoordinatesToPolygon(coordinates):
     if len(coordinates) == 1:
         polygonString = 'POLYGON(('
